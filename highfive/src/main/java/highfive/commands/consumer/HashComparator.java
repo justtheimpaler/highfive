6303c1f70217e0aa8d22b55a732070105a46549e
valarcon <valarcon@example.com>
2025-01-21 22:40:26 -0500

    wip

--- highfive/src/main/java/highfive/commands/consumer/HashComparator.java
+++ highfive/src/main/java/highfive/commands/consumer/HashComparator.java
@@ -1,11 +1,11 @@
 package highfive.commands.consumer;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import highfive.commands.consumer.DumpFileReader.DumpFileIOException;
+import highfive.commands.consumer.DumpFileReader.InvalidDumpFileException;
 import highfive.exceptions.InvalidHashFileException;
 import highfive.model.Hasher;
 
@@ -14,65 +14,17 @@ public class HashComparator implements HashConsumer {
   private static final Logger log = Logger.getLogger(HashComparator.class.getName());
 
   private File baseline;
-  private BufferedReader r;
-
-  private long lineNo;
+  private DumpFileReader r;
   private boolean eof;
-
-  private String hash;
-  private long baselineRow;
 
   private ExecutionStatus status;
 
-  public HashComparator(String tableName, File baseline, File current) throws IOException {
+  public HashComparator(String tableName, File baseline, File current)
+      throws InvalidDumpFileException, DumpFileIOException {
     log.fine("init");
     this.baseline = baseline;
-    this.lineNo = 0;
-    this.eof = false;
-    this.status = null;
-    this.r = new BufferedReader(new FileReader(baseline));
-    readInitialComment(); // skip the first line since it's a comment
-    nextBaseline();
-  }
-  
-  
-
-  private void readInitialComment() throws IOException {
-    String txt = this.r.readLine();
-    if (txt == null) {
-      this.eof = true;
-      this.status = ExecutionStatus.failure(
-          "Could not read the baseline file '" + this.baseline + "'; it does not have the first comment line.");
-    }
-    this.lineNo++;
-  }
-
-  private void nextBaseline() throws IOException {
-    if (this.status != null) {
-      return;
-    }
-    if (this.eof) {
-      this.status = ExecutionStatus
-          .failure("Could not read the baseline file '" + this.baseline + "'; end of file already reached.");
-    } else {
-      String txt = this.r.readLine();
-      if (txt == null) {
-        this.hash = null;
-        this.baselineRow = -1;
-        this.eof = true;
-      } else {
-        this.lineNo++;
-        if (txt.length() < 64 + 1 + 1) {
-          this.hash = null;
-          this.baselineRow = -1;
-          this.status = ExecutionStatus.failure("Could not read the baseline file '" + this.baseline
-              + "'; Invalid format of line #" + this.lineNo + ": " + txt);
-        } else {
-          this.hash = txt.substring(0, 64);
-          this.baselineRow = Long.parseLong(txt.substring(65));
-        }
-      }
-    }
+    this.r = new DumpFileReader(baseline);
+    this.eof = !this.r.next();
   }
 
   @Override
